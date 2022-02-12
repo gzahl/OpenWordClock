2fe74d589e2eeac375c31d5b87c3d34d3d0f843d
Jacob <jacob@jacob.com>
2022-02-12 10:14:46 +0100

    final thoughts from last time

--- wordclock.ino
+++ wordclock.ino
@@ -1,3 +1,13 @@
+#include <DS3231.h>
+#include <Wire.h>
+
+#define CLINT 2
+
+// Setup Clock
+DS3231 Clock;
+bool h12 = false;
+bool pm = false;
+
 typedef enum  {
     ST_CLOCK,
     ST_MENU
@@ -9,9 +19,21 @@ typedef enum  {
 } EVENTS;
 
 void setup() {
+    Clock.setA2Time(0,0,0,0b01111110, false, false, false);
+    Clock.turnOnAlarm(2);
+    Clock.checkIfAlarm(2);
 
+    pinMode(CLINT, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(CLINT), isr_alarm, FALLING);
+
+    Serial.begin(9600);
+    Serial.println("Starting");
 }
 
 void loop() {
 
+}
+
+void isr_alarm() {
+    Serial.println("Interrupted.");
 }
