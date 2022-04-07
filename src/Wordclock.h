e09a7e11c0cedbaed987e1064823615232e5fdad
Jacob <jacob@jacob.com>
2022-04-07 21:26:51 +0200

    Added states showing AM and PM to Wordclock class

--- src/Wordclock.h
+++ src/Wordclock.h
@@ -17,8 +17,11 @@ public:
     static const WordClockState ESIST;
     static const WordClockState MIN5[12];
     static const WordClockState HOURS[12];
-    static const WordClockState UHR;
     static const WordClockState SEC[20];
+    static const WordClockState UHR;
+
+    static const WordClockState AM;
+    static const WordClockState PM;
 
     static const WordClockState MSKMIN5;
     static const WordClockState MSKHOUR;
@@ -28,6 +31,7 @@ public:
     static const WordClockState BRIGHTNESS;
     static const WordClockState TOFF;
     static const WordClockState SET;
+
 
     WordClockState state;
     byte hour;
