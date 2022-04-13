ef0074c30c40960ebb26842de9b94dcd35496079
Jacob <jacob@jacob.com>
2022-04-13 20:35:04 +0200

    Renamed WordClockState TEMP to TEMPERATURE to compy with standard

--- src/Wordclock.h
+++ src/Wordclock.h
@@ -37,7 +37,7 @@ public:
     static const WordClockState BRIGHTNESS;
     static const WordClockState NIGHTMODE;
     static const WordClockState SET;
-    static const WordClockState TEMP;
+    static const WordClockState TEMPERATURE;
 
 
     WordClockState state;
